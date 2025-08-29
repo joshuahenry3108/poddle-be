0a1796c547192e94fa293c21baf085de63dcc36a
joshuahenry3108 <joshua.sahayam@rootquotient.com>
2025-08-29 18:07:57 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const startServer = async () => {
   console.log(process.env);
   await connectDB();
   // Define request response in root URL (/)
-  // Added comment to commit //added another commentline
+  // updating comment line
   app.get("/", function (req, res) {
     res.send("Dockerize the node app changed");
   });
