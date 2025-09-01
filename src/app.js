75c25feca48006bb1c04d1f85eb3a568849dc633
joshuahenry3108 <joshua.sahayam@rootquotient.com>
2025-09-01 18:21:18 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const startServer = async () => {
   });
 
   // Define request response in root URL (/)
-
+  //second commit
   app.get("/health", function (req, res) {
     res.json({
       success: true,
