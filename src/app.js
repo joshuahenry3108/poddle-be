4e7b24314f83008f40d914e754b1f68214834743
joshuahenry3108 <joshua.sahayam@rootquotient.com>
2025-09-01 23:49:29 +0530

    Update app.js test
    updated app.js

--- src/app.js
+++ src/app.js
@@ -8,7 +8,6 @@ const startServer = async () => {
   console.log(process.env);
   await connectDB();
   // Define request response in root URL (/) 
-  //Removed commentline and added that again
   app.get("/", function (req, res) {
     res.send("Dockerize the node app changed");
   });
