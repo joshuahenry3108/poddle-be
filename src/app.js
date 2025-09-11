01b34deeeddff20b2f0ead10036d6650c6dfdc18
joshuahenry3108 <joshua.sahayam@rootquotient.com>
2025-09-11 14:55:55 +0530

    Update app.js for test pipeline

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const startServer = async () => {
   console.log(process.env);
   await connectDB();
   // Define request response in root URL (/) 
-  // hello
+  // updating fule
   app.get("/", function (req, res) {
     res.send("Dockerize the node app changed");
   });
